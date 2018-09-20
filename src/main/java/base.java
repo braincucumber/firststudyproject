27f2be6167c1694d468312ee535a1eaf51d37c72
braincucumber <braincucumber@mail.ru>
2018-09-20 21:48:04 +0300

    Goodbye world

--- src/main/java/base.java
+++ src/main/java/base.java
@@ -1,5 +1,5 @@
 //Created by Evgeniy Busygin on 19.09.2018.
-public class base {
+public class Base {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Goodbye world");
