24add13d799e518ac0eb0e779b0137eaa645f8ef
braincucumber <braincucumber@mail.ru>
2018-10-08 20:14:07 +0300

    Added number randomizer.

--- src/main/java/ArrayLesson4.java
+++ src/main/java/ArrayLesson4.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * This program finds smallest positive and biggest negative number in an array and swaps them.
@@ -8,14 +8,9 @@ import java.util.Scanner;
 
 public class ArrayLesson4 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int[] arrayOfNumbers;
-        arrayOfNumbers = new int[4];
-        for (int i = 0; i < arrayOfNumbers.length; i++) {
-            System.out.println("Enter element №" + (i + 1));
-            // Пользователь вводит количество элементов = длине массива
-            int numberEntry = scanner.nextInt();
-            arrayOfNumbers[i] = numberEntry;
+        int[] arrayOfNumbers = ThreadLocalRandom.current().ints(-10, 10).distinct().limit(20).toArray(); //Случайная генерация массива с уникальными числами
+        for (int i : arrayOfNumbers) {
+            System.out.print(i + " "); // Вывод каждого элемента массива в консоль
         }
         int maxNegElement = -11; //Переменная, отображающая самое большое отрицательное число в массиве (Значение по умолчанию = -11, т.к. все значения в массиве от 10 до -10)
         int maxNegElementPosition = 0; //Позиция этой переменной в массиве
@@ -31,21 +26,15 @@ public class ArrayLesson4 {
                 minPosElementPosition = i;
             }
         }
+        // Меняем элементы местами
         arrayOfNumbers[maxNegElementPosition] = minPosElement;
         arrayOfNumbers[minPosElementPosition] = maxNegElement;
-        // Если переменные не получили новых значений, то операцию невозможно произвести
-        if (maxNegElement != -11 && minPosElement != 11) {
-            System.out.println("Maximal negative element in array is: " + maxNegElement + " on position № " + maxNegElementPosition);
-            System.out.println("Minimal positive elements in array is: " + minPosElement + " on position № " + minPosElementPosition);
-            System.out.println("Array with switched elements :");
-            for (int i : arrayOfNumbers
-            ) {
-                System.out.print(i + " ");
-            }
-        } else if (maxNegElement == -11) { //Если значение переменной осталось -11, значит в массиве отсутствуют отрицательные числа
-            System.out.println("There is no negative elements in array");
-        } else if (minPosElement == 11) { // Если значение переменной осталось 11, значит в массиве отсутствуют положительные числа
-            System.out.println("There is no positive elements in array");
+        System.out.println("\nMaximal negative element in array is: " + maxNegElement + " on position № " + maxNegElementPosition);
+        System.out.println("Minimal positive elements in array is: " + minPosElement + " on position № " + minPosElementPosition);
+        System.out.println("Array with switched elements :");
+        for (int i : arrayOfNumbers
+        ) {
+            System.out.print(i + " ");
         }
     }
 }
