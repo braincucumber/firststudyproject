90f6b34688b20e53b8194f56cd29b754fc8545b1
braincucumber <braincucumber@mail.ru>
2018-10-22 23:22:58 +0300

    Added StringArraySorter.

---
+++ src/main/java/Lesson_6/StringArraySorter.java
@@ -0,0 +1,44 @@
+package Lesson_6;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class StringArraySorter {
+    public static void main(String[] args) {
+        List<String> wordArray = new ArrayList<>();
+        try {
+            for (String file : Files.readAllLines(Paths.get("E:\\IntelliJ IDEA Community Edition 2018.2.4\\IdeaProjects\\src\\main\\java\\Lesson_6\\words.txt"))) {
+                for (String word : file.split("\\s+")) {
+                    wordArray.add(word);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Слова в файле: " + wordArray);
+        wordArray.sort(String.CASE_INSENSITIVE_ORDER);
+        System.out.println("Слова, отсортированные по алфавиту: " + wordArray);
+        int maxCount = 0;
+        String mostFrequentWord = "";
+        HashMap<String, Integer> countingWords = new HashMap<>();
+        for (String word : wordArray) {
+            if (!countingWords.containsKey(word)) {
+                countingWords.put(word, 0);
+            }
+            countingWords.put(word, countingWords.get(word) + 1);
+        }
+        for (String word : countingWords.keySet()) {
+            System.out.println("Cлово \"" + word + "\" встречается " + countingWords.get(word) + " раз");
+            if (maxCount < countingWords.get(word)) {
+                maxCount = countingWords.get(word);
+                mostFrequentWord = word;
+            }
+        }
+        System.out.println("Самое часто повторяющееся слово: \"" + mostFrequentWord + "\" повторяется " + maxCount + " раз");
+
+    }
+}
